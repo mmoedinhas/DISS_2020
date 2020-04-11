bf62b9051ad9924d23f0ad7dc92dffafc9c737e5
Matilde Freilão <mmoedinhas@hotmail.com>
2020-04-11 17:42:14 +0100

    moved createGraph function to helpers folder

--- Story Viewer/controllers/jsonParser.js
+++ Story Viewer/controllers/jsonParser.js
@@ -3,7 +3,8 @@ let router = express.Router();
 let path = require('path');
 let fs = require('fs');
 
-let validateJson = require('../helpers/validateJson.js')
+let validateJson = require('../helpers/validateJson.js');
+let createGraph = require('../helpers/createGraph.js');
 
 router.get('/validate/:filename', function (req, res) {
 
@@ -46,72 +47,5 @@ router.get('/:filename', function (req, res) {
         res.json(graph);
     })
 })
-
-function createGraph(jsonObj) {
-
-    const graph = {
-        nodes: [],
-        edges: []
-    }
-
-    const palette = [
-        '#8ecce6',
-        '#d5cdea',
-        '#fae2ef',
-        '#fcf8e9'
-      ];
-
-      for (scene of jsonObj['scenes']) {
-
-        let x = 0.0;
-        let y = 0.0;
-        let id = 'scene_' + scene['name'];
-
-        graph.nodes.push({
-          id: id,
-          label: scene['name'],
-          type: 'square',
-          x: x,
-          y: y,
-          size: 1,
-          color: palette[0]
-        })
-
-        y += 0.1;
-
-        for (event of scene['events']) {
-
-          let type = 'cross';
-          let color = palette[3];
-          id = "";
-          if(event['type'] == 'cutscene') {
-            type = 'circle';
-            color = palette[1];
-            id = 'cutscene_';
-          } else if(event['type'] == 'gameplay') {
-            type = 'diamond';
-            color = palette[2];
-            id = 'gameplay_';
-          }
-
-          id += event['name'];
-
-          graph.nodes.push({
-            id: id,
-            label: event['name'],
-            type: type,
-            x: x,
-            y: y,
-            size: 0.5,
-            color: color
-          })
-
-          x += 0.1;
-        }
-      }
-
-      return graph;
-}
-
 
 module.exports = router
