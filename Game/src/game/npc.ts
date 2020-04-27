26b322420bf0972a4a151fa5c8e15cc0c38857fe
Matilde Freilão <mmoedinhas@hotmail.com>
2020-04-27 15:22:34 +0100

    drawing npc interaction zones correctly

--- Game/src/game/npc.ts
+++ Game/src/game/npc.ts
@@ -13,7 +13,7 @@ export class Npc extends Actor {
         super(scene, x, y, actorObj);
         (this.sprite.body as Phaser.Physics.Arcade.Body).setImmovable();
         this.interactable = interactable;
-        this.createInteractZone(scene, x, y);
+        this.createInteractZone(scene);
     }
 
     public setInteractable(interactable: boolean) {
@@ -24,14 +24,21 @@ export class Npc extends Actor {
         return this.interactable;
     }
 
-    private createInteractZone(scene: GameScene, x: integer, y: integer) {
+    private createInteractZone(scene: GameScene) {
         const Zone = Phaser.GameObjects.Zone;
-        let mapCoords: ICoordinates = toRealMapCoordinates(x, y, scene.getMap());
 
-        let width = this.sprite.width + scene.getMap().tileWidth*2;
+        let width = this.sprite.width + scene.getMap().tileWidth;
         let height = scene.getMap().tileHeight*2;
 
-        this.interactZone = new Zone(scene, mapCoords.x, mapCoords.y, width, height);
+        let body: Phaser.Physics.Arcade.Body = this.sprite.body as Phaser.Physics.Arcade.Body;
+
+        console.log(body.position);
+        console.log(this.sprite.x + " " + this.sprite.y);
+
+        let x = this.sprite.x - this.sprite.width / 2.0 + body.offset.x + body.width / 2.0;
+        let y = this.sprite.y - this.sprite.height + body.offset.y + body.height / 2.0;
+
+        this.interactZone = new Zone(scene, x, y, width, height);
 
         scene.physics.add.existing(this.interactZone);
     }
