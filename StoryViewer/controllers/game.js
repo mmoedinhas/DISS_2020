59ebcd2c59523172ade63f510d7a263b70d7ae2c
Matilde Freilão <mmoedinhas@hotmail.com>
2020-05-05 12:27:40 +0100

    forgot to delete these files

--- StoryViewer/controllers/game.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let path = require('path');
-
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../views/game.html'));
-})
-
-module.exports = router
