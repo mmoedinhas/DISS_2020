0d96c52b3889887e7c5327d9b3c02ab88dcb844c
Matilde Freilão <mmoedinhas@hotmail.com>
2020-05-05 13:45:20 +0100

    forgot to delete the game controller

--- StoryViewer/controllers/index.js
+++ StoryViewer/controllers/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var path = require('path');
 
 router.use('/validator', require('./validator'))
-router.use('/game', require('./game'))
 router.use('/story-viewer', require('./story-viewer'))
 
 router.get('/', function(req, res) {
