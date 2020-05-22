59ce0c23dab661b44b92d64a067a9229fcdef465
mmoedinhas <mmoedinhas@hotmail.com>
2020-05-22 13:56:17 +0100

    moved js script files

--- Game/src/utils/paths.ts
+++ Game/src/utils/paths.ts
@@ -1,11 +1,11 @@
-export const uiPath: string = 'assets/ui/';
-export const fontsPath: string = 'assets/fonts/';
-export const storyPath: string = 'assets/story/';
-export const eventsPath: string = 'assets/story/event/';
-export const dialoguePath: string = 'assets/story/dialogue/';
-export const mapsPath: string = 'assets/map/';
-export const mapTilesPath: string = 'assets/mapTiles/';
-export const tilesetPath: string = 'assets/tileset/';
+export const uiPath: string = './assets/ui/';
+export const fontsPath: string = './assets/fonts/';
+export const storyPath: string = './assets/story/';
+export const eventsPath: string = './assets/story/event/';
+export const dialoguePath: string = './assets/story/dialogue/';
+export const mapsPath: string = './assets/map/';
+export const mapTilesPath: string = './assets/mapTiles/';
+export const tilesetPath: string = './assets/tileset/';
 
 export function getAssetIdFromPath(path: string): string {
     let slashIndex = (path.lastIndexOf('/') === -1) ? 0 : path.lastIndexOf('/');
