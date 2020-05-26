712892384be2a6518586bc28d4e497dddfac1e97
mmoedinhas <mmoedinhas@hotmail.com>
2020-05-26 15:23:06 +0100

    added db

---
+++ Website/database/connection.php
@@ -0,0 +1,5 @@
+<?php
+  $dbh = new PDO('sqlite:database/diss2020.db');
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+?>
