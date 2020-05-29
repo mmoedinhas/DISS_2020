f5a18e101f73eaaae48d43faa3fd2cce9863c175
mmoedinhas <mmoedinhas@hotmail.com>
2020-05-29 14:15:54 +0100

    add player is redirecting to 404

--- Website/action_add_player.php
+++ Website/action_add_player.php
@@ -5,10 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST' && realpath(__FILE__) == realpath($_SER
      Up to you which header to send, some prefer 404 even if 
      the files does exist for security
   */
-  header('HTTP/1.0 403 Forbidden', TRUE, 404);
-
-  /* choose the appropriate page to redirect users */
-  die(header('location: ./index.php'));
+  include_once('./templates/error.php');
+  die();
 }
 
 include_once('database/db_diss2020.php');
